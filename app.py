82781750f9ad35e5f96502fb05f4a3bc6d74a413
Acka1357 <Acka1357@gmail.com>
2018-08-03 03:49:37 -0700

    fail to json

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import json, urllib.request
 
 app = Flask(__name__)
 
@@ -18,6 +19,49 @@ def api():
 @app.route('/demo')
 def demo():
     return render_template("demo.html")
+
+
+@app.route('/api/image', methods=['POST'])
+def api_image():
+	print(request.data)
+	req_url = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases'
+	req = urllib.request.Request(req_url)
+	# req_head = {
+	# 	'Content-type' : 'application/json',
+	# 	'Ocp-Apim-Subscription-Key': 'e53a58d375cd465489d65e0d471dbedb'
+	# }
+	req.add_header('Content-type', 'application/json')
+	req.add_header('Ocp-Apim-Subscription-Key', 'e53a58d375cd465489d65e0d471dbedb')
+	req_body = {
+			  "documents": [
+			    {
+			      "language": "en",
+			      "id": "1",
+			      "text": request.data
+			    }
+			  ]
+			}
+	jsondata = json.dumps(req_body)
+	jsondataasbytes = jsondata.encode('utf-8')
+	req.add_header('Content-Length', len(jsondataasbytes))
+	print(jsondataasbytes)
+	# req_head = json.dumps(req_head)
+	# req_body = jsonify(documents=json.dumps(req_body))
+
+	# print(req_head)
+	# print(req_body)
+	# res = requests.post(,
+	# 			 headers=req_head)
+	# , json=req_body)
+	res = urllib.request.ulropen(req, jsondataasbytes)
+	print(res)
+	return jsonify(result=search_image())
+
+
+def search_image(keywords):
+	# code here
+
+	return 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
 
 
 if __name__ == '__main__':
--- static/js/api.js
+++ static/js/api.js
@@ -15,16 +15,33 @@ btnText.addEventListener('click', function(){
     contentText.style.display = 'block';
 });
 
-// function openFunc(evt, FuncName) {
-//     var i, x, tablinks;
-//     x = document.getElementById("content-text");
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.visibility = "none";
-//     }
-//     tablinks = document.getElementsByClassName("tablink");
-//     for (i = 0; i < x.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-//     }
-//     document.getElementById(FuncName).style.visibility = "block";
-//     evt.currentTarget.className += " w3-red";
-// }
+
+var btnSummary = document.getElementById('btn-test-summary');
+var btnImage = document.getElementById('btn-test-image');
+var inputText = document.getElementById('input-test');
+var modalContentSummary = document.getElementById('modal-content-summary');
+var modalContentImage = document.getElementById('modal-content-image')
+btnSummary.addEventListener('click', function(){
+	modalContentSummary.innerHTML = "<p>Please wait for summarization.</p>";
+	// var request = new XMLHttpRequest();
+	// request.open('POST', '/test_api', true);
+	// request.onload = function () {
+	//   // Begin accessing JSON data here
+	//   var data = JSON.parse(this.response);
+	//   console.log(data);
+	//   modalContentSummary.innerHTML = "";
+	// }
+	// request.send('abdc');
+});
+
+btnImage.addEventListener('click', function(){
+	modalContentImage.innerHTML = "<p>Please wait for search.</p>";
+	var request = new XMLHttpRequest();
+	request.open('POST', '/api/image', true);
+	request.onload = function () {
+	  var img_url = JSON.parse(this.response);
+	  console.log(img_url);
+	  modalContentImage.innerHTML = '<img src="' + img_url + '" class="modal-content modal-img"></img>';
+	}
+	request.send(inputText.value);
+});
